3d2bbf347778e56c909bf91c678e3b6b7c1bbb2c
Phạm Lê Anh Tuấn <phamleanhtuan16022005@gmail.com>
2025-10-03 16:21:23 +0700

    Add files via upload

---
+++ Layout/src/main/java/com/example/layout/LayoutApplication.java
@@ -0,0 +1,11 @@
+package com.example.layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutApplication.class, args);
+    }
+}
