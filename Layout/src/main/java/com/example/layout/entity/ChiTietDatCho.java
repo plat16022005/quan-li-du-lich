df572b378a1b9131fe77fbd3986537490f2f1a7a
Luuloneranger <luuga000@gmail.com>
2025-10-09 14:43:16 +0700

    update entity lần 3

--- Layout/src/main/java/com/example/layout/entity/ChiTietDatCho.java
+++ Layout/src/main/java/com/example/layout/entity/ChiTietDatCho.java
@@ -26,6 +26,55 @@ public class ChiTietDatCho {
 
     @Column(name = "LoaiVe", length = 50)
     private String loaiVe;
+
+	public Integer getMaChiTiet() {
+		return maChiTiet;
+	}
+
+	public void setMaChiTiet(Integer maChiTiet) {
+		this.maChiTiet = maChiTiet;
+	}
+
+	public DatCho getDatCho() {
+		return datCho;
+	}
+
+	public void setDatCho(DatCho datCho) {
+		this.datCho = datCho;
+	}
+
+	public Integer getSoLuong() {
+		return soLuong;
+	}
+
+	public void setSoLuong(Integer soLuong) {
+		this.soLuong = soLuong;
+	}
+
+	public BigDecimal getDonGia() {
+		return donGia;
+	}
+
+	public void setDonGia(BigDecimal donGia) {
+		this.donGia = donGia;
+	}
+
+	public BigDecimal getThanhTien() {
+		return thanhTien;
+	}
+
+	public void setThanhTien(BigDecimal thanhTien) {
+		this.thanhTien = thanhTien;
+	}
+
+	public String getLoaiVe() {
+		return loaiVe;
+	}
+
+	public void setLoaiVe(String loaiVe) {
+		this.loaiVe = loaiVe;
+	}
     
     // Getters and setters
+    
 }
