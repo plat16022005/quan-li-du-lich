efe96739167a47b5626a4b6370be9c1540b682eb
Phạm Lê Anh Tuấn <phamleanhtuan16022005@gmail.com>
2025-10-20 14:51:29 +0700

    Backup

--- Layout/src/main/java/com/example/layout/entity/ChuyenDuLich.java
+++ Layout/src/main/java/com/example/layout/entity/ChuyenDuLich.java
@@ -8,6 +8,8 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 //import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "ChuyenDuLich")
 public class ChuyenDuLich {
@@ -17,6 +19,7 @@ public class ChuyenDuLich {
     private Integer maChuyen;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JsonIgnore 
     @JoinColumn(name = "MaTour")
     private Tour tour;
 
